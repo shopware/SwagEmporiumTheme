dddfb7cc533394b8cd18b44f040555b4378ae393
Thu.Vo <thu.vo@shopware.com>
2022-04-25 12:03:39 +0700

    EMT-36 - Filter property select preview

---
+++ Resources/app/storefront/src/plugin/listing/emt-filter-rating-select.plugin.js
@@ -0,0 +1,35 @@
+import FilterRatingSelectPlugin from 'src/plugin/listing/filter-rating-select.plugin'
+import DomAccess from 'src/helper/dom-access.helper';
+
+export default class EmtFilterRatingSelectPlugin extends FilterRatingSelectPlugin {
+
+    /**
+     * add data currentRatingPercent to labels
+     * @return {Array}
+     * @public
+     */
+    getLabels() {
+        const currentRating = DomAccess.querySelector(this.el, this.options.checkboxSelector + ':checked', false).value;
+
+        let labels = [];
+
+        if (currentRating) {
+            let endSnippet = this.options.snippets.filterRatingActiveLabelEnd;
+            if (parseInt(currentRating) === 1) {
+                endSnippet = this.options.snippets.filterRatingActiveLabelEndSingular;
+            }
+
+            labels.push({
+                label: `${this.options.snippets.filterRatingActiveLabelStart}
+                        ${currentRating}/${this.options.maxPoints}
+                        ${endSnippet}`,
+                id: 'rating',
+                currentRatingPercent: parseInt(currentRating/this.options.maxPoints*100)
+            });
+        } else {
+            labels = [];
+        }
+
+        return labels;
+    }
+}
---
+++ Resources/app/storefront/src/plugin/listing/emt-listing.plugin.js
@@ -0,0 +1,39 @@
+import deepmerge from 'deepmerge';
+import ListingPlugin from 'src/plugin/listing/listing.plugin';
+
+export default class EmtListingPlugin extends ListingPlugin {
+    /**
+     * Template for an active filter label and check label rating to load icon star
+     *
+     * @param {Object} label
+     * @returns {string}
+     */
+    getLabelTemplate(label) {
+        if(label.id == 'rating') {
+            return `
+            <span class="${this.options.activeFilterLabelClass}">
+                <div class="filter-active-icon" title="${label.currentRatingPercent}%">
+                     <span style="width:${label.currentRatingPercent}%">
+                         <span>${label.label}</span>
+                     </span>
+                </div>
+                <button class="${this.options.activeFilterLabelRemoveClass}"
+                        data-id="${label.id}">
+                    &times;
+                </button>
+            </span>
+            `;
+        } else {
+            return `
+        <span class="${this.options.activeFilterLabelClass}">
+            ${this.getLabelPreviewTemplate(label)}
+            ${label.label}
+            <button class="${this.options.activeFilterLabelRemoveClass}"
+                    data-id="${label.id}">
+                &times;
+            </button>
+        </span>
+        `;
+        }
+    }
+}
